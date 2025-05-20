3c8ebaf677b57cfa9999922e21b0581b803aaf28
Alani Rigotti de Oliveira <alani.oliveira@proway.treina>
2025-05-20 11:51:25 -0300

    commit inicial do app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void  main() {
+  runApp(Container(
+    child: Text(
+      "Bem vindo ao flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.amber)
+    )
+  )); // grande widget
+}
